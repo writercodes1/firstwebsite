3c254920e9c9f0d5ff3a31ccc1c6ef2e4b31942b
writecoder67 <rajsoni6080as@gmail.com>
2023-09-12 17:42:42 +0530

    add files

---
+++ index.js
@@ -0,0 +1,29 @@
+function call(){
+
+    
+const rotated = document.getElementById("para");
+
+// Rotate element by 180 degrees clockwise
+
+rotated.style.transform = "rotate(300deg)";
+}
+
+function call1(){
+
+    
+    const rotated = document.getElementById("para");
+    
+    // Rotate element by 180 degrees clockwise
+    
+    rotated.style.transform = "rotate(30deg)";
+    }
+
+  
+function call2(){
+
+    var x = document.getElementById("qq").value;
+    const h= document.getElementById("h111");
+  h.innerHTML= x;
+  document.getElementById("qq").value="";
+    }
+  
